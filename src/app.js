739f30058bb2bafc81a9fed31abf5925e4c7c3fc
SwarjanRaj <swaraj@emobmo.com>
2025-08-04 17:42:12 +0530

    Finish merge - add render workflow

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const app = express();
 
 const morgan = require('morgan');
 const cors = require('cors');
+app.use(cors({
+  origin: ['https://dashbaordfrontend.onrender.com', 'http://localhost:5173'],
+  credentials: true
+}));
+
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
@@ -22,11 +27,6 @@ if (process.env.NODE_ENV === 'development') {
 // Set security headers
 app.use(helmet());
 // Allow CORS from your frontend
-app.use(cors({
-  origin: "*", // For dev, or use specific domain like "https://yourfrontend.onrender.com"
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
 
 // Body parser
 app.use(express.json());
@@ -46,10 +46,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // Enable CORS
-app.use(cors({
-  origin: 'http://localhost:5173', // must match your frontend origin exactly
-  credentials: true
-}));
+
 
 // Mount routers
 app.use('/api/v1/auth', authRoutes);
@@ -61,3 +58,6 @@ app.use("/api/v1/modules", fieldRoutes);
 app.use(errorHandler);
 
 module.exports = app;
+
+
+
